76c8078a57e6d56127dab282ea46a64c71126fb3
Usman <usman.mughal@outlook.fr>
2023-01-16 21:55:25 +0100

    création d'un fonction générale pour les inputs champs formulaire

--- js/modalForm.js
+++ js/modalForm.js
@@ -1,139 +1,98 @@
-// DECLARATION DE VARIABLE FIRST & FIRSTNAMERROR
+//Récupération des variables des differents champs du formulauire
 let firstName = document.querySelector("#first");
-let firstNameError = document.querySelector("firstNameError");
-
-// CONDITION REGEX: FIRSTNAME
-function firstNameControl() {
-
-    if (/^([A-Za-z]{2,20})?([-]{0,1})?([A-Za-z]{2,20})$/.test(firstName.value)) {
-        firstName.style.border = "solid 2px green";
-        firstNameError.style.color = 'green';
-
-        return true;
-    }
-
-    else {
-        firstName.style.border = "solid 2px red";
-        firstNameError.style.color = 'red';
-
-        return false;
-    }
-}
-
-// EVENEMENT D'ECOUTE POUR LE CONTROLE DE L'INPUT FIRSTNAME
-firstName.addEventListener('change', () => {
-    firstNameControl();
-});
-
-
-
-// DECLARATION DE VARIABLE LASTNAME & LASTNAMEERROR
 let lastName = document.querySelector("#last");
-let lastNameError = document.querySelector("#lastNameError");
-
-// CONDITION REGEX: LASTNAME
-function lastNameControl() {
-
-    if (/^([A-Za-z]{2,20})?([-]{0,1})?([A-Za-z]{2,20})$/.test(lastName.value)) {
-        lastName.style.border = "solid 2px green";
-        lastNameError.style.color = 'green';
-
-        return true;
-    }
-
-    else {
-        lastName.style.border = "solid 2px red";
-        lastNameError.style.color = 'red';
-
-        return false;
-    }
-
-}
-
-
-// EVENEMENT D'ECOUTE POUR LE CONTROLE DE L'INPUT LASTNAME
-lastName.addEventListener('change', () => {
-    lastNameControl();
-});
-
-
-// DECLARATION DE VARIABLE EMAIL & EMAILERROR
 let email = document.querySelector("#email");
-let emailError = document.querySelector("#emailError");
-
-// CONDITION REGEX: LASTNAME
-function emailControl() {
-
-    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.value)) {
-        email.style.border = "solid 2px green";
-
-        return true;
-    }
-
-    else {
-        email.style.border = "solid 2px red";
-
-        return false;
-    }
-}
-
-
-// EVENEMENT D'ECOUTE POUR LE CONTROLE DE L'INPUT EMAIL
-email.addEventListener('change', () => {
-    emailControl();
-});
-
-
-// DECLARATION DE VARIABLE BIRTHDATE & BIRTHDATEERROR
-let birthdate = document.querySelector("#brithdate");
-let birthdateError = document.querySelector("#birthdateError");
-
-// CONDITION REGEX: CHAMP DE DATE
-function birthdateControl() {
-
-    if (/^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/.test(birthdate.value)) {
-        birthdate.style.border = "solid 2px green";
-
-        return true;
-    }
-
-    else {
-        birthdate.style.border = "solid 2px red";
-
-        return false;
-    }
-
-}
-
-// EVENEMENT D'ECOUTE POUR LE CONTROLE DE L'INPUT BIRTHDATE
-birthdate.addEventListener('change', () => {
-    birthdateControl();
-});
-
-// DECLARATION DE VARIABLE QUANTITY & QUANTITYERROR
+let birthdate = document.querySelector("#birthdate");
 let quantity = document.querySelector("#quantity");
+let checkLocation = document.getElementsByName("location");
+let checkbox1 = document.querySelector("#checkbox1");
+
+
+//Récupération des champs d'erreur du formulaire
+let firstNameError = document.querySelector("#firstNameError");
+let lastNameError = document.querySelector("#lastNameError");
+let emailError = document.querySelector("#emailError");
+let birthdateError = document.querySelector("#birthdateError");
 let quantityError = document.querySelector("#quantityError");
+let locationError = document.querySelector("#locationError");
+let conditionCheckboxError = document.querySelector("#generalCheckboxError");
 
-// CONDITION REGEX: CHAMPS QUANTITY
-function quantityControl() {
-
-    if (quantity.value >= 0) {
-        quantity.style.border = "solid 2px green";
-
+//fonction générale pour les input firstName lastName Email
+function validation(input, error, regex, validMessage, invalidMessage) {
+    if (regex.test(input.value)) {
+        input.style.border = "solid 2px green";
+        error.textContent = validMessage;
+        error.style.color = 'green';
+        error.style.fontSize = '15px';
+        error.style.marginBottom = '5px';
         return true;
-    }
-
-
-    else {
-        quantity.style.border = "solid 2px red";
-
+    } else {
+        input.style.border = "solid 2px red";
+        error.textContent = invalidMessage;
+        error.style.color = 'red';
+        error.style.fontSize = '15px';
+        error.style.marginBottom = '5px';
         return false;
     }
 }
 
-
-
-// EVENEMENT D'ECOUTE POUR LE CONTROLE DE L'INPUT QUANTITY
-quantity.addEventListener('change', () => {
-    quantityControl();
+// Evenement d'écoute sur les differents inputs du formulaire
+firstName.addEventListener('change', () => {
+    validation(firstName, firstNameError, /^[a-zA-Z]{2,20}$/, "Champ Valide", "Veuillez saisir deux caractères minimums");
 });
+
+lastName.addEventListener('change', () => {
+    validation(lastName, lastNameError, /^[a-zA-Z]{2,20}$/, "Champ Valide", "Veuillez saisir deux caractères minimums");
+});
+
+email.addEventListener('change', () => {
+    validation(email, emailError, /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Champ Valide", "Veuillez saisir une adresse email valide");
+});
+
+birthdate.addEventListener('change', () => {
+    validation(birthdate, birthdateError, /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/, "Champ Valide", "Veuillez saisir votre date de naissance");
+});
+
+quantity.addEventListener('change', () => {
+    validation(quantity, quantityError, /^[0-9]+$/, "Champ Valide", "Merci d'indiquer le nombre de tournois");
+});
+
+function locationChecking() {
+    let isChecked = false;
+    for (let i = 0; i < checkLocation.length; i++) {
+        if (checkLocation[i].checked) {
+            isChecked = true;
+            break;
+        }
+    }
+
+    if (isChecked) {
+        locationError.textContent = "Champ valide";
+        locationError.style.color = 'green';
+        locationError.style.fontSize = '15px';
+        locationError.style.marginBottom = '5px';
+        return true;
+    } else {
+        locationError.textContent = "Merci de sélectionner une localisation";
+        locationError.style.color = 'red';
+        locationError.style.fontSize = '15px';
+        locationError.style.marginBottom = '5px';
+        return false;
+    }
+}
+
+checkLocation.forEach((checkLocationInput) => checkLocationInput.addEventListener('change', locationChecking));
+
+checkbox1.addEventListener('change', () => {
+    if (!checkbox1.checked) {
+        conditionCheckboxError.textContent = "Merci d'accepter les conditions d'utilisations";
+        conditionCheckboxError.style.color = 'red';
+        conditionCheckboxError.style.fontSize = '15px';
+        conditionCheckboxError.style.marginBottom = '5px';
+    } else {
+        conditionCheckboxError.textContent = "Champs Valide";
+        conditionCheckboxError.style.color = 'green';
+        conditionCheckboxError.style.fontSize = '15px';
+        conditionCheckboxError.style.marginBottom = '5px';
+    }
+})
